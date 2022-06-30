70a537cac6a6676e784b311f670ac94b5b33bcef
Brightlaz <lazbright1@gmail.com>
2022-06-30 16:08:20 -0700

    Download button
    I am working on the download button fom html to pdf

--- assets/js/createNarrative.js
+++ assets/js/createNarrative.js
@@ -14,3 +14,27 @@ write.addEventListener('click', () => {
     input.classList.remove('off');
     output.classList.add('off');
 })
+
+const button = document.querySelector('#button')
+button.addEventListener('click', () => {
+    // var element = document.getElementById('canvas_div_pdf');
+    html2pdf(output, {
+        margin: 10,
+        filename: 'create.pdf',
+        image: {
+            type: 'jpeg',
+            quality: 0.98
+        },
+        html2canvas: {
+            scale: 2,
+            logging: true,
+            dpi: 192,
+            letterRendering: true
+        },
+        jsPDF: {
+            unit: 'mm',
+            format: 'a4',
+            orientation: 'portrait'
+        }
+    });
+})
