fa2f955e7454d33d725d5c98d21d9590315de5a9
Brightlaz <lazbright1@gmail.com>
2022-07-19 16:20:18 -0700

    Updates

---
+++ precache-manifest.67756b2fbf1bcca0b0d81e237138c96b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "6731c2c50e59426aafd5",
+    "url": "/Imaginative-narrative/static/css/main.faf17b1b.chunk.css"
+  },
+  {
+    "revision": "6731c2c50e59426aafd5",
+    "url": "/Imaginative-narrative/static/js/main.6731c2c5.chunk.js"
+  },
+  {
+    "revision": "b45957439db38050202c",
+    "url": "/Imaginative-narrative/static/js/1.b4595743.chunk.js"
+  },
+  {
+    "revision": "6e4cba002d4ad49e70e2",
+    "url": "/Imaginative-narrative/static/js/2.6e4cba00.chunk.js"
+  },
+  {
+    "revision": "a5112fd56c3e449ba2d5",
+    "url": "/Imaginative-narrative/static/js/3.a5112fd5.chunk.js"
+  },
+  {
+    "revision": "0111d869c28ed3d09cb6",
+    "url": "/Imaginative-narrative/static/js/4.0111d869.chunk.js"
+  },
+  {
+    "revision": "94256f0c9f8e2b81b4d5",
+    "url": "/Imaginative-narrative/static/js/5.94256f0c.chunk.js"
+  },
+  {
+    "revision": "f140db6c4fb216799708",
+    "url": "/Imaginative-narrative/static/js/runtime~main.f140db6c.js"
+  },
+  {
+    "revision": "cbcacd1cbcd43a02653722efe5983f52",
+    "url": "/Imaginative-narrative/static/media/small.cbcacd1c.svg"
+  },
+  {
+    "revision": "5717dbc4ecd85358840ff34691d1bea0",
+    "url": "/Imaginative-narrative/static/media/shape.5717dbc4.svg"
+  },
+  {
+    "revision": "c663b055d1efd88cbbe85775af97edba",
+    "url": "/Imaginative-narrative/index.html"
+  }
+];
